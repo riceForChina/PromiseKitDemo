3209002b024e88dbd01d12881f144b09d04fb247
fanqile <fanqile@xcyo.com>
2018-01-05 16:30:14 +0800

    Initial Commit

---
+++ Promise-OCTests/Promise_OCTests.m
@@ -0,0 +1,39 @@
+//
+//  Promise_OCTests.m
+//  Promise-OCTests
+//
+//  Created by fanqile on 2018/1/5.
+//  Copyright © 2018年 fanqile. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Promise_OCTests : XCTestCase
+
+@end
+
+@implementation Promise_OCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
